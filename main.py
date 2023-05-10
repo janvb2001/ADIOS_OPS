32ce8e2fd6b006eca36fd0056f0e3991da9eca7b
Jan van Buuren <jan.van.buuren.2001@hotmail.com>
2023-05-10 17:48:25 +0200

    driving van

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import random as rnd
 import time
 from math import sqrt
+import time
 
 # Things to include:
 # - Drones should have a delay at the ground station and each litter piece (Which can be deduced from req.)
@@ -13,156 +14,281 @@ from math import sqrt
 # - Include different litter types in the program
 # - Include different drone types in the program
 # - Include obstacles
-# - Include the possibility for the van to drive
 # - Include reconnaisance drone
 # - Wind
 
+# Done:
+# - Include the possibility for the van to drive
 
-#inputs
-litter_n = 100
+# inputs
+litter_n = 1000
 drone_n = 10
-v_drone = 20 # m/s
+v_drone = 8    # m/s
+v_van = 2       # m/s
+vanMovement = 1         # 0: Stationary van, 1: van squares around
+plot_driveplan = True
 
-dt = 1
+runspeed = 100          # Factor wrt real-time
 
-# coordinates in m
+dt = 1  # s
+
+# sizes of area in m
 x_size = 1000
 y_size = 1000
 
-#starting position of ground station
-ground_stat_pos = [int(x_size/2), int(y_size/2)]
+# starting position of ground station
 
+# Van starting position
+if vanMovement == 0:
+    #Starts in middle
+    ground_stat_pos = [int(x_size/2), int(y_size/2)]
+elif vanMovement == 1:
+    x_clearance = 1/10 * x_size
+    y_clearance = 1/10 * y_size
 
-#function to determine distance between 2 points
+    ly = y_size - y_clearance
+    lx = x_size - x_clearance
+
+    ground_stat_pos = [int(x_clearance), int(y_clearance)]
+    van_positions = [[ground_stat_pos[0], ground_stat_pos[0]],
+                     [ground_stat_pos[1], ground_stat_pos[1] + ly - y_clearance]]
+
+    dir = 2             # 1: up, 2: right, 3: down, 4: left
+    cont = True
+    while cont:
+        if dir == 1:
+            ly = ly - y_clearance
+            van_positions[0].append(van_positions[0][-1])
+            van_positions[1].append(van_positions[1][-1] + ly)
+        elif dir == 2:
+            lx = lx - x_clearance
+            van_positions[0].append(van_positions[0][-1] + lx)
+            van_positions[1].append(van_positions[1][-1])
+        elif dir == 3:
+            ly = ly - y_clearance
+            van_positions[0].append(van_positions[0][-1])
+            van_positions[1].append(van_positions[1][-1] - ly)
+        elif dir == 4:
+            lx = lx - x_clearance
+            van_positions[0].append(van_positions[0][-1] - lx)
+            van_positions[1].append(van_positions[1][-1])
+            dir = 0
+
+        if lx < 0 or ly < 0:
+            cont = False
+        dir += 1
+
+# function to determine distance between 2 points
 def dist(x1, x2, y1, y2):
     return sqrt((x2-x1)**2 + (y2-y1)**2)
 
-#Determine the new coordinate from current coordinate, target and speed
+# Determine the new coordinate from current coordinate, target and speed
 def newcoor(finx, finy, curx, cury, v, dt):
+    # distance to destination
     dcur = dist(curx, finx, cury, finy)
 
+    # distance traveled in this timestep
     d_traveled = v * dt
 
+    # Destination is reached
     if d_traveled > dcur:
         return finx, finy
+    # destination is not reached
     else:
+        # fraction of total distance that is traveled in this timestep
         fracd = d_traveled / dcur
+        # multiply this fraction with total distance to travel and find new coordinates of the drone
         newx = curx + (finx - curx)*fracd
         newy = cury + (finy - cury)*fracd
 
         return newx, newy
 
-#Setting up the drone data arrays
+def new_d_t(curx, cury, finx, finy, v):
+    dn = dist(curx, finx, cury, finy)
+    t_b = dn / v
+    return t_b, dn
+
+def on_route(curx, cury, goalx, goaly, v, dt):
+    newx, newy = newcoor(goalx, goaly, curx, cury, v, dt)
+
+    # recalculate the time needed to reach destination and store
+    t_b, dn = new_d_t(curx, cury, goalx, goaly, v)
+
+    return newx, newy, t_b, dn
+def van_reached(lit_a):
+
+    # Check whether there is still litter left to be taken
+    if len(lit_a[0]) > 0:
+        # Choose a litter item
+            # Randomly
+        # nextitemi = rnd.randint(0, len(lit_a[0]) - 1)
+        # nextitem = lit_a[0][nextitemi]
+
+            # Closest to van
+        mindis = min(lit_a[1])
+        nexti = lit_a[1].index(mindis)
+        nextitem = lit_a[0][nexti]
+
+        # Remove the chosen litter item from the available list
+        liti = lit_a[0].index(nextitem)
+        lit_a[0].pop(liti)
+        lit_a[1].pop(liti)
+
+        # Calculate the time needed to get to the destination and store
+        t_busy, dnext = new_d_t(drone_data[0][d], drone_data[1][d], littercoor[0][nextitem],
+                                littercoor[1][nextitem], v_drone)
+
+    else:
+        # if there is no litter left, the drone is done
+        nextitem = -2
+        t_busy = None
+
+    return nextitem, t_busy, lit_a
+
+def recalc_lit_dist(lit_coor, lit_a, gs_x, gs_y):
+    distances = []
+
+    for li in range(len(lit_a[0])):
+        di = dist(gs_x, lit_coor[0][lit_a[0][li]], gs_y, lit_coor[1][lit_a[0][li]])
+        distances.append(di)
+
+    return distances
+
+def plot_update(newxdrone, newydrone, newxvan, newyvan, newxlit, newylit):
+    # updating the plot
+    litterplot.set_xdata(newxlit)
+    litterplot.set_ydata(newylit)
+
+    groundplot.set_xdata(newxvan)
+    groundplot.set_ydata(newyvan)
+
+    droneplot.set_xdata(newxdrone)
+    droneplot.set_ydata(newydrone)
+
+    figure.canvas.draw()
+    figure.canvas.flush_events()
+
+
+
+# Setting up the drone data arrays
 # x_coor, y_coor, end time busy, item traveling to
 drone_data = [[], [], [], []]
+active_drones = []
 for d in range(drone_n):
     drone_data[0].append(ground_stat_pos[0])
     drone_data[1].append(ground_stat_pos[1])
     drone_data[2].append(0)
-    drone_data[3].append(0)
+    drone_data[3].append(-1)
+
+    active_drones.append(d)
 
 # Setting up the litter data arrays
-lit_avail = []
-litterdata = []
+lit_avail = [[],[]]
 littercoor = [[],[]]
 for i in range(litter_n):
-    lit_avail.append(i)
-    litterdata.append([])
-    littercoor[0].append(rnd.randint(0,x_size))
-    littercoor[1].append(rnd.randint(0, y_size))
+    lit_avail[0].append(i)
 
-#to run GUI event loop, necessary for animation
+    xlit = rnd.randint(0, x_size)
+    ylit = rnd.randint(0, y_size)
+    littercoor[0].append(xlit)
+    littercoor[1].append(ylit)
+
+    d_van = dist(xlit, ground_stat_pos[0], ylit, ground_stat_pos[1])
+    lit_avail[1].append(d_van)
+
+# to run GUI event loop, necessary for animation
 plt.ion()
 
-#setting up de figure with how each object is displayed
+# setting up de figure with how each object is displayed
 figure, ax = plt.subplots(figsize=(10, 8))
 litterplot, = plt.plot(littercoor[0], littercoor[1], 'o', color='black', markersize = 4)
 groundplot, = plt.plot(ground_stat_pos[0], ground_stat_pos[1], color='g', marker='s', markersize=10)
 droneplot, = plt.plot(drone_data[0], drone_data[1], 'o', color='b', markersize = 6)
 
-#variable to keep track of how many drones are done
-dronesDone = 0
+if plot_driveplan:
+    checkpoint, = plt.plot(van_positions[0], van_positions[1])
+
+startTime = time.time()
 
 run = True
 t = 0
+cur_goal = 1
+totaldronedist = 0
 while run:
+    while t < runspeed * (time.time()-startTime):
+        # loop over every drone
+        for d in active_drones:
+            litteri = drone_data[3][d]
 
-    #loop over every drone
-    for d in range(drone_n):
-        #check whether the drone is not done
-        if drone_data[3][d] != -2:
-            #check whether a drone has reached its destination
-            if drone_data[2][d] <= t:
-                #Check whether a drone needs a new litter target or needs to return to ground station
-                if drone_data[3][d] == -1 or t==0:
-                    #Check whether there is still litter left to be taken
-                    if len(lit_avail)>0:
-                        #Choose a litter item
-                        nextitemi = rnd.randint(0,len(lit_avail)-1)
-                        nextitem = lit_avail[nextitemi]
+            # The drone is done with getting to the van
+            if drone_data[2][d] <= t and drone_data[3][d] == -1:
 
-                        #Remove the chosen litter item from the available list
-                        liti = lit_avail.index(nextitem)
-                        lit_avail.pop(liti)
-                        drone_data[3][d] = nextitem
+                #Selection of next item
+                nextitem, t_busy, lit_avail = van_reached(lit_avail)
+                drone_data[3][d] = nextitem
 
-                        #Calculate the time needed to get to the destination and store
-                        dnext = dist(drone_data[0][d], littercoor[0][nextitem], drone_data[1][d], littercoor[1][nextitem])
-                        t_busy = dnext/v_drone
-                        drone_data[2][d] = t + t_busy
-                    else:
-                        #if there is no litter left, the drone is done
-                        drone_data[3][d] = -2
-                        dronesDone += 1
+                # If there is no more litter, remove the drone from active drones
+                if nextitem == -2:
+                    dronei = active_drones.index(d)
+                    active_drones.pop(dronei)
 
+                    drone_data[0][d] = None
+                    drone_data[1][d] = None
                 else:
-                    #The litter is picked up so has no coordinate anymore
-                    littercoor[0][drone_data[3][d]] = None
-                    littercoor[1][drone_data[3][d]] = None
-
-                    #The drone is no longer on its way to litter
-                    drone_data[3][d] = -1
-
-                    #Calculate and store the distance and time needed to get to the ground station
-                    dnext = dist(drone_data[0][d], ground_stat_pos[0], drone_data[1][d], ground_stat_pos[1])
-                    t_busy = dnext / v_drone
                     drone_data[2][d] = t + t_busy
-            else:
-                # The drone continues further in the direction of its destination.
-                litteri = drone_data[3][d]
-                #Continue in the direction of the van
+
+            # The drone has reached the litter piece
+            elif drone_data[2][d] <= t and drone_data[3][d] >= 0:
+                # The litter is picked up so has no coordinate anymore
+                littercoor[0][drone_data[3][d]] = None
+                littercoor[1][drone_data[3][d]] = None
+
+                # The drone is no longer on its way to litter
+                drone_data[3][d] = -1
+
+                # Determine the time needed to reach destination and store
+                t_busy, dnext = new_d_t(drone_data[0][d], drone_data[1][d], ground_stat_pos[0], ground_stat_pos[1],
+                                        v_drone)
+                drone_data[2][d] = t + t_busy
+
+
+            # If the drone is on its way
+            elif drone_data[2][d] > t:
+                # Headed to van
                 if litteri == -1:
-                    nx, ny = newcoor(ground_stat_pos[0], ground_stat_pos[1], drone_data[0][d], drone_data[1][d],
-                                     v_drone, dt)
+                    # The drone is on its way to the van
+                    nx, ny, t_busy, dis = on_route(drone_data[0][d], drone_data[1][d], ground_stat_pos[0],
+                                              ground_stat_pos[1], v_drone, dt)
+                # Headed to litter
+                elif litteri >= 0:
+                    # The drone is on its way to a litter piece
+                    nx, ny, t_busy, dis = on_route(drone_data[0][d], drone_data[1][d], littercoor[0][litteri],
+                                              littercoor[1][litteri], v_drone, dt)
 
-                    dnext = dist(drone_data[0][d], ground_stat_pos[0], drone_data[1][d], ground_stat_pos[1])
-                    t_busy = dnext / v_drone
-                    drone_data[2][d] = t + t_busy
-                #Continue in the direction of the litter
-                else:
-                    nx, ny = newcoor(littercoor[0][litteri], littercoor[1][litteri], drone_data[0][d], drone_data[1][d], v_drone, dt)
-
-                    dnext = dist(drone_data[0][d], littercoor[0][drone_data[3][d]], drone_data[1][d], littercoor[1][drone_data[3][d]])
-                    t_busy = dnext / v_drone
-                    drone_data[2][d] = t + t_busy
-
-                #The new coordinates of the drone is the calculated coordinates
+                # Store data
+                totaldronedist += dis
+                drone_data[2][d] = t + t_busy
                 drone_data[0][d] = nx
                 drone_data[1][d] = ny
 
+        if vanMovement == 1:
+            ground_stat_pos[0], ground_stat_pos[1], t_busy, dis = on_route(ground_stat_pos[0], ground_stat_pos[1], van_positions[0][cur_goal], van_positions[1][cur_goal], v_van, dt)
+            lit_avail[1] = recalc_lit_dist(littercoor, lit_avail, ground_stat_pos[0], ground_stat_pos[1])
 
-    # updating the plot
-    litterplot.set_xdata(littercoor[0])
-    litterplot.set_ydata(littercoor[1])
 
-    droneplot.set_xdata(drone_data[0])
-    droneplot.set_ydata(drone_data[1])
+            if ground_stat_pos[0] == van_positions[0][cur_goal] and ground_stat_pos[1] == van_positions[1][cur_goal]:
+                cur_goal += 1
 
-    figure.canvas.draw()
-    figure.canvas.flush_events()
+        # check whether simulation is done
+        if len(active_drones) == 0:
+            time.sleep(3)
+            run = False
+            print("Total drone distance is: " + str(totaldronedist))
+            print("End time is: " + str(t))
+            break
 
-    if dronesDone == drone_n:
-        time.sleep(3)
-        run = False
-    #time.sleep(0.05)
-    t += dt
+        t += dt
+
+    plot_update(drone_data[0], drone_data[1], ground_stat_pos[0], ground_stat_pos[1], littercoor[0], littercoor[1])
+
+
