ca0257627e58eb31fa0b56167ed5790762e0f6d7
Aryan Nokhai <a.nokhai@topdesk.com>
2023-06-08 11:41:33 +0200

    Created function that created a 1D spline between 2 points

--- OPS_SIM/minimum_snap_path_planner.py
+++ OPS_SIM/minimum_snap_path_planner.py
@@ -13,38 +13,30 @@ def get_A(t0, t1):
 
 def get_polynomial_from_coeffs(p_array):
 
-    t_array = lambda t: np.array([float(t)**7, float(t)**6, float(t)**5, float(t)**4, float(t)**3, float(t)**2, float(t), 1])
-    x_t = lambda t: np.dot(t_array(t), p_array)
+    t_array = lambda t: np.array([t**7, t**6, t**5, t**4, t**3, t**2, t, np.ones(len(t))])
+    x_t = lambda t: np.dot(np.transpose(t_array(t)), p_array)
+
     return x_t
 
 # Takes the desired states at certain locations as input, gives a trajectory as a polynomial function of time as output
-def create_path():
-
+def create_spline(b, t0, t1):
 
     # x(t) = p7 * t^7 + p6 * t^6 + p5 * t^5 + p4 t^4 + p3 * t^3 + p2 * t^2 + p1 * t + p0
 
-    b = np.transpose(np.matrix([0, 0, 0, 0, 1, 0, 0, 0]))
-
-    A = get_A(0, 1)
+    A = get_A(t0, t1)
     p_array = np.linalg.inv(A) * b
-
-    print(p_array)
-
-    # x_t = lambda t: p_array[0] * t**7 + p_array[1] * t**6 + p_array[2] * t**5 + p_array[3] * t**4 + \
-    #                p_array[4] * t**3 + p_array[5] * t**2 + p_array[6] * t + p_array[7]
 
     x_t = get_polynomial_from_coeffs(p_array)
 
-    t_array = np.arange(0, 1, 0.1)
+    t_array = np.arange(t0, t1, 0.01)
     x_array = x_t(t_array)
-
-    print(t_array)
-    print(x_array)
 
     plt.plot(t_array, x_array)
     plt.show()
 
-create_path()
+
+b = np.transpose(np.matrix([1, -1, 0, 0, 3, -1, 0, 0]))
+create_spline(b, 3, 4)
 
 
 
