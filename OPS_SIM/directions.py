26cafca092ac2940eb4bd78b94750d3403566f2d
Aryan Nokhai <a.nokhai@topdesk.com>
2023-06-08 14:41:11 +0200

    Added enum for a star directions, renamed the entries

--- OPS_SIM/directions.py
+++ OPS_SIM/directions.py
@@ -1,12 +1,13 @@
 from enum import Enum
 
 
-class Direction(Enum):
-    N = 1
-    NE = 2
-    E = 3
-    SE = 4
-    S = 5
-    SW = 6
-    W = 7
-    NW = 8
+class Directions(Enum):
+    NORTH = 1
+    NORTH_EAST = 2
+    EAST = 3
+    SOUTH_EAST = 4
+    SOUTH = 5
+    SOUTH_WEST= 6
+    WEST = 7
+    NORTH_WEST = 8
+    KANYE_WEST = 9
