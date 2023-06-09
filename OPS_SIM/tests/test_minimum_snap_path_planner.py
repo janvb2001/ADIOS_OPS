957be7d23d56c80f00894a4ef5186c808724869e
Aryan Nokhai <a.nokhai@topdesk.com>
2023-06-09 14:44:10 +0200

    Wrote unit test to test the get velocity and accel function

--- OPS_SIM/tests/test_minimum_snap_path_planner.py
+++ OPS_SIM/tests/test_minimum_snap_path_planner.py
@@ -56,3 +56,36 @@ class test_minimum_snap_path_planner(unittest.TestCase):
 
             with self.subTest():
                 self.assertEqual(expected_directions_values[i], actual_direction)
+
+    # Acceleration not tested yet as it is subject to tweaking
+    def test_get_velocity_and_accel_from_direction(self):
+
+        # has the shape [[direction, direction_change, speed, block_size] ... []]
+        parameter_array = np.array([[0, 0, 10, 1],
+                                    [3, 1, 8, 2],
+                                    [6, 3, 6, 1],
+                                    [7, 3, 9, 1],
+                                    [2, 2, 4, 4]])
+
+        # has the shape [[vx, vy] ... [vx, vy]]
+        expected_velocity_array = np.array([[0, 10],
+                                            [0.5*np.sqrt(2) * 8, -0.5*np.sqrt(2) * 8],
+                                            [-6, 0],
+                                            [-0.5*np.sqrt(2) * 9, 0.5*np.sqrt(2) * 9],
+                                            [4, 0]])
+
+        for i in range(len(parameter_array)):
+            direction = parameter_array[i][0]
+            direction_change = parameter_array[i][1]
+            speed = parameter_array[i][2]
+            block_size = parameter_array[i][3]
+
+            actual_vx, actual_vy, ax, ay = minimum_snap_path_planner.get_velocity_and_accel_from_direction(direction, direction_change, speed, block_size)
+            expected_vx, expected_vy = expected_velocity_array[i][0], expected_velocity_array[i][1]
+
+            print(expected_vx, actual_vx, expected_vy, actual_vy)
+            with self.subTest():
+                self.assertEqual(expected_vx, actual_vx)
+            with self.subTest():
+                self.assertEqual(expected_vy, actual_vy)
+
